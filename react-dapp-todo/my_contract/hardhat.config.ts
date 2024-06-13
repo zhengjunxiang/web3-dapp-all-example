02b0a6d1eac91bdfc79288ab55ba05aee71dd534
zhengjunxiang <zhengjunxiang92@gmail.com>
2024-06-14 00:33:35 +0800

    feat: update

--- react-dapp-todo/my_contract/hardhat.config.ts
+++ react-dapp-todo/my_contract/hardhat.config.ts
@@ -20,8 +20,7 @@ const config: HardhatUserConfig = {
     ganache: {
       url: "http://127.0.0.1:8545",
       accounts: [
-        "0xb33048cdde31d7ecc86c22a62cccf2d9348fc48209d5340c5a79c5995b226a60",
-        "0x8b2bb2e302a24d1bf4c926045ec6fb4d7a9b9fa3469bd437da4bf027dc420645",
+        "0xba63461fde9443e654ce3447e3dc875e05763568a1434829deb0d578df810a89",
       ],
     },
   },
